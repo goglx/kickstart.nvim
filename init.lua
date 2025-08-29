3e416a3cd623f7b748a493983caee593ae202bf2
lambovg <georgi.lambov@gmail.com>
2025-08-29 22:03:45 +0300

    fix: disable default Ctrl+n and Ctrl+p keymaps in blink-cmp config

--- init.lua
+++ init.lua
@@ -1079,6 +1079,9 @@ require('lazy').setup({
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
+        ['<C-n>'] = {},
+        ['<C-p>'] = {},
+
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
