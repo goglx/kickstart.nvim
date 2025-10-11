0bae462801619f4890cc079e88c15b7755815900
lambovg <georgi.lambov@gmail.com>
2025-10-11 12:00:28 +0300

    fix(init.lua): fix telescope find_files keymap to use correct cwd when using tabs

--- init.lua
+++ init.lua
@@ -599,7 +599,13 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+
+      -- because of setting wrong cwd when using tabs 
+      vim.keymap.set('n', '<leader>sf', function()
+        local root = vim.lsp.buf.list_workspace_folders()[1] or vim.fn.getcwd()
+        require('telescope.builtin').find_files { cwd = root }
+      end, { desc = '[S]earch [F]iles' })
+
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
