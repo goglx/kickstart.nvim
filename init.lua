eb7c20eda04147869f14b171e1b559a517747032
lambovg <georgi.lambov@gmail.com>
2026-04-07 22:36:27 +0300

    fix(init.lua): enable gopls, eslint, yamlls for lsp configuration
    This commit enables the `gopls`, `eslint`, and `yamlls` language servers for LSP (Language Server Protocol) configuration in Neovim. This will improve support for Go, JavaScript/TypeScript, and YAML files respectively within the editor.

--- init.lua
+++ init.lua
@@ -1050,7 +1050,7 @@ require('lazy').setup({
       ---@type table<string, vim.lsp.Config>
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         --
@@ -1058,7 +1058,10 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        buf_ls = {},
+        eslint = {},
+        yamlls = {},
 
         stylua = {}, -- Used to format Lua code
 
